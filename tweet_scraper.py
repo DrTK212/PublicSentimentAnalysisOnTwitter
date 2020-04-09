9353188a027819bfef40d0c6a85ee431deb5416f
Ryutaro Takanami <49274608+DrTK212@users.noreply.github.com>
2020-04-09 16:17:34 +0900

    Update tweet_scraper.py

--- tweet_scraper.py
+++ tweet_scraper.py
@@ -20,18 +20,13 @@ from pymongo import MongoClient
 import pprint as pp
 from collections import OrderedDict
 
-filename = '/Users/RyutaroTakanami/Desktop/tweets.csv'
+filename = '/Users/パス/tweets.csv'
 client = MongoClient('localhost',27017)
 db = client['TweetDB']
 TWEET_NUM = 1000
 
-#音夢
-#'https://twitter.com/search?l=ja&q=%E7%94%B1%E5%A4%A2%20since%3A2017-09-01%20until%3A2017-09-02&src=typd'
-#2016/1/1
-#'https://twitter.com/search?l=ja&q=since%3A2016-01-01%20until%3A2016-01-02&src=typd'
-#2016/1/3
-#https://twitter.com/search?l=ja&q=since%3A2016-01-03%20until%3A2016-01-04&src=typd&lang=ja
-path_to_chromedriver = '/usr/local/bin/chromedriver'            # change path as needed
+
+path_to_chromedriver = '/usr/local/bin/chromedriver'          
 url = 'https://twitter.com/search?l=ja&q=since%3A2016-12-31%20until%3A2017-01-01&src=typd'
 browser = webdriver.Chrome(executable_path = path_to_chromedriver)
 
@@ -65,7 +60,7 @@ def twt_scroller(url):
 
 
     
-#function to handle/parse HTML and extract data - using BeautifulSoup    
+  
 def blogxtract(url):
     
     #regex patterns
@@ -80,18 +75,13 @@ def blogxtract(url):
 
     blog_list = []
      
-    #set to global in case you want to play around with the HTML later   
+  
     global soup    
     
-    #call dynamic page scroll function here
+
     soup = BeautifulSoup(twt_scroller(url), "html.parser")
     tweet_num = TWEET_NUM
-    """
-    phour = '15'
-    """
-    
-    
-    print('aaaaaaaaaaaaaaaaa')
+
     
         
     for i in soup.find_all('li', {"data-item-type":"tweet"}):
@@ -99,20 +89,8 @@ def blogxtract(url):
             tweet_num -= 1
             
             date = (i.small.a['title'] if i.small is not None else "")
-            """
-            nhour = date[0]+date[1]
-            nhour = re.sub(':','',nhour)
-            
-            
-            
-            
-            if nhour == str(phour):
-            """
-                
-                    
-                
-            
-                
+                 
+ 
                 
             user = (i.div['data-name'] if i.div is not None else "")
             text = (i.p.get_text() if i.p is not None else "")
@@ -120,20 +98,9 @@ def blogxtract(url):
 
             
             
-            """
-            #build dictionary
-            blog_dict = {
-                    "number": 1000-tweet_num, 
-                    "user": user,
-                    "date": date,
-                    "blog_text": problemchars.sub('', url_finder.sub('', text)),
-                    }
-        
-          
-            blog_list.append(blog_dict)
-            """
+
             db.tweets1231.insert_one({
-                    "number": 1000-tweet_num, 
+                    "number": 1000-tweet_num, #一日につき1000件のツイートを取得
                     "user": user,
                     "date": date,
                     "blog_text": problemchars.sub('', url_finder.sub('', text))
@@ -144,24 +111,7 @@ def blogxtract(url):
             if tweet_num <= 0:
                 break
                 
-            """
-                tweet_num -= 1
-                print(tweet_num)
-                if tweet_num <= 0:
-                    phour = int(phour)-1
-                    tweet_num = TWEET_NUM
-                    print(phour)
-            
- 
-                if phour < 0:
-                        print('ccccccc')
-                        phour = 23
-                
-                
-            else:
-                print('aaaaaa')
-                continue
-            """
+
     #error handling  
         except (AttributeError, TypeError, KeyError, ValueError):
             print("missing_value")
@@ -169,41 +119,14 @@ def blogxtract(url):
             continue
     
     
-    """
-    #call csv writer function and output file
-    writer_csv_3(blog_list)
-    """
+
     
     
     
     return 
 
 
-"""
-#pp.pprint(blog_list[0:2])
-    
-#function to write CSV file
-def writer_csv_3(blog_list):
-    
-    #uses group name from URL to construct output file name
-    file_out = filename.format(page = url.rsplit('/',2)[1])
-    
-    with open(file_out, 'w') as csvfile:
-
-        writer = csv.writer(csvfile, lineterminator='\n', delimiter=',', quotechar='"')
-    
-        for i in blog_list:
-            if len(i['blog_text']) > 0:
-                newrow = i['number'],i['user'], i['date'], i['blog_text']
-
-                writer.writerow(newrow)                     
-            else:
-                pass
-                
-"""
-    
-#tip the domino
 if __name__ == "__main__":
     blogxtract(url)
-    print("\007")
+
 
